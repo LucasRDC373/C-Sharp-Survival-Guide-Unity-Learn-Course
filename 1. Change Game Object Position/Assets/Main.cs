90044fdd123f7099f8848416637cd383daf98c18
LucasRDC373 <62164610+LucasRDC373@users.noreply.github.com>
2020-03-29 01:43:38 -0300

    Fixed a bug in the organization.

---
+++ 1. Change Game Object Position/Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    public Vector3 startPotition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // component.property = asign new value ;
+        transform.position = startPotition; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
