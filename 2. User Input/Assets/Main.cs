68fcadce5c8395c8d3db162a78434131034a7408
LucasRDC373 <62164610+LucasRDC373@users.noreply.github.com>
2020-03-29 04:32:04 -0300

    We added you saw it in new classes
    We added the seen in new classes like:
    
    - User Input
    - Simple Movement

---
+++ 2. User Input/Assets/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If we use "GetJeyDown" the fuction is executed when key is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Space");
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            print("E");
+        }
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            print("F");
+        }
+    }
+}
