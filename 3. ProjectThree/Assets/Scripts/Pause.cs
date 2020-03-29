23d7c6a4623e03119e47f7e5c95ed17d26a6d981
LucasRDC373 <62164610+LucasRDC373@users.noreply.github.com>
2020-03-29 17:12:41 -0300

    The nomenclature was changed and new exercises were added

---
+++ 3. ProjectThree/Assets/Scripts/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0; 
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    
+}
